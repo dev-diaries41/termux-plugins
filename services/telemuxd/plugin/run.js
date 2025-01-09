0662e8a00a081c227a1ce20c9e1253733ebbb48e
d41dev <d41dev@proton.me>
2025-01-09 17:32:00 +0000

    update imports for telemuxd

--- services/telemuxd/plugin/run.js
+++ services/telemuxd/plugin/run.js
@@ -1,5 +1,5 @@
-import { getBot } from "./src/bot";
-import { getEventHandlers } from "./src/bot";
+const { getBot } = require("./src/bot") ;
+const { getEventHandlers }= require( "./src/bot");
 
 const bot = getBot()
 bot.start(getEventHandlers(bot))
